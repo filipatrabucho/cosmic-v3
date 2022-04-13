a7e6f99ff6e77055ee0462b93008269ebd6d2377
filipatrabucho <filipatrabucho@gmail.com>
2022-04-13 15:09:22 +0100

    add footer

--- src/component/graphicspage/graphics.js
+++ src/component/graphicspage/graphics.js
@@ -7,7 +7,6 @@ import "./graphicspage.css";
 function GraphicsPage({ title, text, img, button }) {
   return (
     <div className="graphics">
-
       <div className="graphics__container">
         <hr class="graphics__line" />
         <div className="graphics__flex">
