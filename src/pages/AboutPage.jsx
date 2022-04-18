195a16d7951507bc2e3fb3fb6d94074f44d65f77
filipatrabucho <filipatrabucho@gmail.com>
2022-04-18 12:54:49 +0100

    create hiring page consts

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import About from "../component/about/about";
 import icon from "../assets/about_icon.png";
-import "../../src/component/about/about.css";
 import Routemap from "../component/routemap/routemap";
 import aboutimg from "../assets/aboutus_banner.png";
+import mobileBanner from "../assets/vertical_banner.png";
+import "../../src/component/about/about.css";
+
 function AboutPage() {
   const routes = [
     {
@@ -18,19 +20,24 @@ function AboutPage() {
   return (
     <>
       <img className="about__banner" alt="About Banner" src={aboutimg} />
+      <img
+        className="about__mobilebanner"
+        src={mobileBanner}
+        alt="About Vertical Banner"
+      />
       <Routemap routes={routes} />
       <div className="about">
         <h2 className="about__title">Everywhere Team</h2>
         <div className="about__line"></div>
         <h3 className="about__subtitle">Who we are?</h3>
 
-        <p>
+        <p className="about__text">
           _Everywhere Team has been developing and innovating within the
           cryptocurrency and blockchain world since early 2021. We are a
           passionate team with highly skilled individuals that saw the potential
           and added value of being early birds in this massive game changer.
         </p>
-        <p>
+        <p className="about__text">
           This is more than a 9 to 5 job for us.. We really care about what we
           do and put a lot of time into making sure everything we touch shines.
           This is what puts the bread on the table, pays our rents, makes us
