d13518f81546393d53e3a1ecd8d396b155c11621
Sérgio Santos <sergiosantos2612@gmail.com>
2022-04-18 09:54:36 +0100

    fixed issues with merge

--- src/component/banner/banner.js
+++ src/component/banner/banner.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 import banner from '../../assets/banner 3 with gradient 1.png';
-<<<<<<< HEAD
-=======
+
 import banner_mobile from '../../assets/section 1.png';
->>>>>>> 364167a58d36a35c6f8a70cd7e8571b62d172a7a
+
 import './banner.css';
 
 function Banner() {
