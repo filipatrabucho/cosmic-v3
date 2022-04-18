129110560f9f5db82173b447d0e1b7c4c8c37b85
Sérgio Santos <sergiosantos2612@gmail.com>
2022-04-18 15:23:55 +0100

    fixed contact page

--- src/component/banner/banner.js
+++ src/component/banner/banner.js
@@ -1,18 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import './banner.css';
-
-function Banner() {
-    return (
-      <div className="banner__container">
-      </div>
-    );
-  };
-
-  export default Banner;
-  
-  
-=======
 import "./banner.css";
 
 function Banner() {
@@ -20,4 +6,3 @@ function Banner() {
 }
 
 export default Banner;
->>>>>>> 98632cc0840ffe6fb484e4212eba1836fa1e4923
