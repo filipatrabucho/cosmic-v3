fd9465a593c1ce2111b976f65825b06c1c515513
Sérgio Santos <sergiosantos2612@gmail.com>
2022-04-18 12:19:42 +0100

    add styles and responsive to contact page

--- src/pages/ContactusPage.jsx
+++ src/pages/ContactusPage.jsx
@@ -8,14 +8,15 @@ function ContactusPage() {
       <Banner />
       <div className="contact">
         <h2 className="contact__title">Contact Us</h2>
-
-        <div className="contact__line"></div>
-
-        <p>
+        <div className="graphics__line"></div>
+        <p className="contact__description">
           We really care about what you have to say, if you wish to reach out to
           us feel free to do so below!
         </p>
-
+        <p className="contact__responsive">
+        At Cosmic Exodus, accessible from cosmicexodus.finance, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Cosmic Exodus and how we use it.
+        </p>
+        <div className="contact__line-responsive"></div>
         <form action="" method="post">
           <input className="contact__input" type="text" placeholder="Name" />
           <input className="contact__input" type="email" placeholder="Email" />
@@ -24,13 +25,14 @@ function ContactusPage() {
             type="text"
             placeholder="Message"
           />
+          <div className="contact__flex">
           <p className="contact__info">
             By clicking “submit”. You agree to be subscribed to Cosmic Exodus
             newsletter according to our{" "}
             <a href="/privacypage">privacy policy</a>.
           </p>
-
-          <button type="submit">Submit</button>
+          <button className="contact__submit" type="submit">Submit</button>
+          </div>
         </form>
       </div>
     </>
