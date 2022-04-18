6c5ae2541e7ad0fc08cac1ec9b46c6efeac387ec
filipatrabucho <filipatrabucho@gmail.com>
2022-04-18 10:03:08 +0100

    fix the changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./component/navbar/navbar";
 import Banner from "./component/banner/banner";
 import Routemap from "./component/routemap/routemap";
-<<<<<<< HEAD
-import logo from "./assets/COSMIC-EXODUS-FINAL 2.png";
-import content from "./assets/256 256 1.svg";
-import profilePic from "./assets/THANK WITHOU BG 1.svg";
-import Footer from './component/footer/footer';
-=======
 import Footer from "./component/footer/footer";
 import GraphicsPage from "./pages/GraphicsPage";
 import ContentPage from "./pages/ContentPage";
 import ProfilePage from "./pages/ProfilePage";
 import HiringPage from "./pages/HiringPage";
->>>>>>> 364167a58d36a35c6f8a70cd7e8571b62d172a7a
+import "./App.css";
 
 function App() {
   return (
